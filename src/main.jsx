2cd0430efef0ddf4c0706c04032ee3d4c6f9e6a0
smackpalmer <zachpalmer00@gmail.com>
2026-06-07 22:16:55 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ClerkProvider } from '@clerk/clerk-react'
 import App from './App.jsx'
-
-const PUBLISHABLE_KEY = 'pk_test_d2lyZWQtcmVkYmlyZC02Ni5jbGVyay5hY2NvdW50cy5kZXYk'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
-    </ClerkProvider>
+    <App />
   </React.StrictMode>
 )
