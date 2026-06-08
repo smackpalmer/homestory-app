fe01bf35e759b0a7d8f48ba35c2054cdf949f07b
smackpalmer <zachpalmer00@gmail.com>
2026-06-08 09:36:33 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useRef, useEffect } from "react";
-import { useAuth, useUser, SignIn } from "@clerk/clerk-react";
 
 
 // ── Theme ─────────────────────────────────────────────────────────────────────
